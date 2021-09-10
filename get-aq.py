d5fd932ef9d2c6b821fdeb88e75636661208bb0e
Wesley Graba <wesley.graba@gmail.com>
2021-09-10 17:03:35 -0600

    Working "get list of monitors" within distance from postal code

--- get-aq.py
+++ get-aq.py
@@ -2,7 +2,9 @@
 
 
 import argparse
+import datetime
 import enum
+from json.decoder import JSONDecodeError
 import geopy
 import geopy.geocoders
 import geopy.distance
@@ -61,10 +63,12 @@ class ConcUnits(enum.Enum):
 
 
 class Monitor(NamedTuple):
+    name: str
     type: MonitorType
     loc: geopy.Point
     aqi: int
-    conc: int
+    conc: float
+    raw_conc: float
     conc_units: ConcUnits
 
 
@@ -90,13 +94,70 @@ def get_monitors(
     :param loc: Location to search from
     :param dist_mi: Distance in miles to search from loc
     :param session: Request session with API key defaulted
-    :reutrn: List of Monitors
+    :return: List of Monitors
     """
     monitors = []
     min_point = geopy.distance.distance(miles=dist_mi).destination(loc, bearing=225)
     max_point = geopy.distance.distance(miles=dist_mi).destination(loc, bearing=45)
 
-    # session.get(AIRNOW_API_DATA_QUERY_URL, {})
+    # https://www.airnowapi.org/aq/data/?startDate=2021-09-10T22&endDate=2021-09-10T23&parameters=OZONE,PM25,PM10&BBOX=-106.171227,39.144701,-103.556480,40.427867&dataType=B&format=application/json&verbose=0&monitorType=0&includerawconcentrations=0&API_KEY=20089F4D-6621-46B0-9046-98860751C5E9
+    rsp = session.get(
+        AIRNOW_API_DATA_QUERY_URL,
+        params={
+            "parameters": "OZONE,PM25,PM10",  # O3, PM2.5, PM10.0
+            "bbox": f"{min_point.longitude},{min_point.latitude},{max_point.longitude},{max_point.latitude}",
+            "monitortype": 0,  # Permanent monitors only
+            "datatype": "B",  # Concentrations & AQI
+            "format": "application/json",
+            "verbose": 1,
+            "includerawconcentrations": 1,
+        },
+    )
+    raw_monitors = None
+    try:
+        rsp.raise_for_status()
+        raw_monitors = rsp.json()
+
+    except (requests.HTTPError, JSONDecodeError) as err:
+        logger.error(f"Error in response: {err}")
+
+    if raw_monitors:
+        for raw_mon in raw_monitors:
+            # Parse Monitor Type
+            mon_type = None
+            if raw_mon["Parameter"] == "OZONE":
+                mon_type = MonitorType.OZONE
+
+            elif raw_mon["Parameter"] == "PM2.5":
+                mon_type = MonitorType.PM2p5
+
+            elif raw_mon["Parameter"] == "PM10":
+                mon_type = MonitorType.PM10
+
+            if not mon_type:
+                raise ValueError(f"Unknown Monitor Type {raw_mon['Parameter']}")
+
+            # Parse Concentration Units
+            conc_units = None
+            if raw_mon["Unit"] == "UG/M3":
+                conc_units = ConcUnits.UG_M3
+
+            elif raw_mon["Unit"] == "PPB":
+                conc_units = ConcUnits.PPB
+
+            if not conc_units:
+                raise ValueError(f"Unkown Concentation Units {raw_mon['Unit']}")
+
+            monitor = Monitor(
+                name=raw_mon["SiteName"],
+                type=mon_type,
+                loc=geopy.Point(raw_mon["Latitude"], raw_mon["Longitude"]),
+                aqi=raw_mon["AQI"],
+                conc=raw_mon["Value"],
+                raw_conc=raw_mon["RawConcentration"],
+                conc_units=conc_units,
+            )
+            monitors.append(monitor)
 
     return monitors
 
@@ -130,7 +191,7 @@ if __name__ == "__main__":
     )
 
     sess = requests.Session()
-    sess.headers.update({"API_KEY": cli_args.api_key})
+    sess.params = {"api_key": cli_args.api_key}
 
     geocoder = SimpleGeocoder()
     loc = geocoder.get_loc(cli_args.postalcode)
